41951b70a36e94246153d0b2ad90d987b570327a
Ayoub <a_bousslama@hetic.eu>
2025-11-18 09:31:39 +0100

    description

---
+++ sources/__init__.py
@@ -0,0 +1,7 @@
+"""
+Package sources :
+- classifier.py : classification Groq
+- gmail_reader.py : lecture Gmail via OAuth
+- sheets_writer.py : écriture Google Sheets
+- main.py : pipeline principal
+"""
